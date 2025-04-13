b7c5f37460e73ffa06a9119822da1b6b521599b0
Luis Martínez <lmartinezquijandria03@gmail.com>
2025-04-13 11:36:38 -0500

    Revert "Merge pull request #2 from Alyri03/feature/pagina-principal-e-informativas"
    This reverts commit 416239b11d775d9b322c1a1f92422968bc59072a, reversing
    changes made to 5e291d6ff75c996709fd30903b0aa31f24a0e33a.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import React from 'react';
-import AppRoutes from '@/routes/Routes.jsx';
-// Si no tienes Header aún, comenta esta línea
-// import Header from '@/components/Header.jsx';
-
-const App = () => {
+function App() {
   return (
-    <>
-      {/* <Header /> */}
-      <AppRoutes />
-    </>
+    <div className="bg-red-500 text-white p-6">
+      <h1 className="text-2xl mb-4">¿Tailwind está funcionando?</h1>
+      <button className="bg-[var(--color-cuaternario)] text-white px-4 py-2">
+        Botón con color primario
+      </button>
+    </div>
   );
-};
+}
 
 export default App;
--- src/pages/PaginaPrincipal/components/Carrusel.jsx
+++
@@ -1,9 +0,0 @@
-const Carrusel = () => {
-
-    return (
-        <>
-        </>
-    )
-}
-
-export default Carrusel;
