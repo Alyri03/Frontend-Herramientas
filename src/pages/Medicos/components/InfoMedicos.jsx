4f228dba24b723ae3aa2e38b998120f77788446a
JuanGarcia <juan2525.2004@gmail.com>
2025-04-15 23:00:05 -0500

    feat: Avance de 'InfoMedicos'

--- src/pages/Medicos/components/InfoMedicos.jsx
+++ src/pages/Medicos/components/InfoMedicos.jsx
@@ -1,26 +1,34 @@
-
+import ImagenDoctorGenerica from '../../../assets/images/ImagenDoctorGenerica.jpg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLongArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 const InfoMedicos = () => {
-    return(<>
+    return (<>
         <section className="pt-10">
-            <div className="container">
-
-                <div className="item 1">
-                    <div className="img">
-                        <img href="" alt="doctor" />
-                    </div>
-                    <div>
-                        <div>
-                            <p><b>Cardiologia</b></p>
+            <div className="flex container w-full h-auto bg-cyan-500">
+                {/* Item1 */}
+                <div className="w-75 h-auto bg-yellow-500">
+                    
+                        <div className="relative">
+                            <img src={ImagenDoctorGenerica} alt="doctor" className='rounded-4xl'/>
                         </div>
-                        <div>
-                            <p><b>Dr. Martinez Quijandria Luis</b></p>
-                            <p>CMP: 55124</p>
+                        <div className="flex flex-col rounded-4xl bg-stone-50 p-5">
+                            <div className='rounded-2xl bg-sky-100 p-2 w-33 text-center'>
+                                <p><b>Cardiologia</b></p>
+                            </div>
+                            <div className='pt-2'>
+                                <p><b>Dr. Martinez Quijandria Luis</b></p>
+                                <p className='pt-2 text-stone-400'>CMP: 55124</p>
+                            </div>
+                            <div className='flex pt-5 justify-center'>
+                                <div className='flex justify-center items-center'>
+                                    <a href="" className='text-blue-800'>
+                                        <b className='pr-2'>Conoce al medico</b>
+                                        <FontAwesomeIcon icon={faLongArrowRight} className='text-blue-800'/>
+                                    </a>
+                                </div>
+                            </div>
                         </div>
-                        <div>
-                            <a href="">Conoce al medico</a>
-                        </div>
-                    </div>
                 </div>
 
             </div>
