3c841f972526af713339f5631459b21ed016366d
JuanGarcia <juan2525.2004@gmail.com>
2025-04-17 23:56:30 -0500

    feat: retoques en InfoMedicos y falta responsive en DetalleMedicos

--- src/pages/Medicos/DetalleMedicos.jsx
+++ src/pages/Medicos/DetalleMedicos.jsx
@@ -1,72 +1,86 @@
+import ImagenDoctorGenerica from '../../assets/images/ImagenDoctorGenerica.jpg'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLongArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
 const DetalleMedico = () => {
-    return(<>
-        <section>
-            <div className="flex w-full gap-5 container mx-auto" >
-                <div className="flex flex-col bg-yellow-900 w-3xl justify-center items-center">
-                    <div className="bg-yellow-400 h-full">
-                       <img src="https://droscargaona.com/wp-content/uploads/Imagen-1.jpg" alt="doctor"/>
-                    </div>
-                    <div className="flex gap-5 bg-white w-full">
-                        <div>
-                            <FontAwesomeIcon icon={faLongArrowLeft}/>
-                        </div>
-                        <div>
-                            <p>Volver al listado</p>
-                        </div>
-                        <div>
-                            <p>Reservar Cita</p>
-                        </div>
+    return (<>
+        <section className='pt-10 pb-10'>
+            <div className="flex justify-center gap-10">
+                {/* Imagen | Nav */}
+                <div className="flex flex-col w-3xl justify-center items-center relative">
+                    <img src={ImagenDoctorGenerica} alt="doctor" className="w-full object-cover" />
+                    <div className="absolute bottom-0 left-0 w-full flex justify-between items-center border border-stone-200 bg-white border-t rounded-t-3xl px-5 py-3 z-10">
+                        <a href="/equipo-medico" className="flex items-center text-blue-900 font-medium">
+                            <FontAwesomeIcon icon={faLongArrowLeft} className="mr-2" />
+                            <p className='text-lg'>Volver al listado</p>
+                        </a>
+                        <a href="#" className="bg-cyan-700 text-white rounded-full w-45 h-10 flex justify-center items-center font-semibold hover:bg-cyan-800 transition text-lg">
+                            Reservar Cita
+                        </a>
                     </div>
                 </div>
-                <div className='bg-yellow-600 w-[50%] h-full'>
-                    <div>
-                        <p><b>Dr Reyes Pérez, Alvaro Orlando</b></p>
-                        <p>CMP: 55124</p>
-                        <p>RNE: 032319</p>
+
+                {/* Info Doctor */}
+                <div className='w-[45%] h-auto'>
+                    <div className='flex flex-col'>
+                        <p className='text-6xl pt-2'><b>Dr. Reyes Pérez, Alvaro Orlando</b></p>
+                        <div className='flex gap-20 pt-3 pb-3'>
+                            <p className='text-2xl text-gray-400'>CMP: 55124</p>
+                            <p className='text-2xl text-gray-400'>RNE: 032319</p>
+                        </div>
                     </div>
-                    <div>
-                        <p><b>Especialidades</b></p>
-                        <p>Cardiologia</p>
+                    {/* Especialidades */}
+                    <div className='pt-3 pb-3'>
+                        <p className='text-3xl'><b>Especialidades</b></p>
+                        <div className='pt-3'>
+                            <p className="bg-blue-100 rounded-full w-45 h-10 flex justify-center items-center font-semibold text-lg">
+                                Cardiología
+                            </p>
+                        </div>
                     </div>
-                    <div>
-                        <p><b>Sobre mí</b></p>
-                        <p>Soy miembro de La Sociedad Estadounidense de Cirujanos Plásticos. A lo largo de mi carrera en el campo de la cirugía plástica y estética, he participado en una amplia gama de actividades educativas y eventos científicos entre las cuales destacan:</p>
-                        <ul>
+                    {/* Sobre mí */}
+                    <div className='pt-3 pb-3'>
+                        <p className='text-3xl'><b>Sobre mí</b></p>
+                        <p className='text-xl pt-3'>Soy miembro de La Sociedad Estadounidense de Cirujanos Plásticos. A lo largo de mi carrera en el campo de la cirugía plástica y estética, he participado en una amplia gama de actividades educativas y eventos científicos entre las cuales destacan:</p>
+                        <ul className='text-xl pt-3 list-disc list-outside pl-10'>
                             <li>Anatomy of Aging Bitesized y Clinical Practice</li>
                             <li>Transplante facial de La Sociedad Estadounidense de Cirujanos Plásticos.</li>
                         </ul>
                     </div>
-                    <div>
-                        <p><b>Formación</b></p>
-                        <ul>
+                    {/* Formación */}
+                    <div className='pt-3 pb-3'>
+                        <p className='text-3xl'><b>Formación</b></p>
+                        <ul className='text-xl pt-3 list-disc list-outside pl-10'>
                             <li>Médico Cirujano de Novgorod State University. (2002-2008)</li>
                             <li>Cirujano Plástico, Estético y Reconstructivo de la Universidad de Ciencias Médicas de La Habana. (2013-2017)</li>
                         </ul>
                     </div>
-                    <div>
-                        <p><b>Horarios</b></p>
-                        <ul className="">
-                            <li>
-                                <p>Martes</p>
-                                <p>16:00 - 17:00</p>
+                    {/* Horarios */}
+                    <div className='pt-3 pb-3'>
+                        <p className='text-3xl'><b>Horarios</b></p>
+                        <ul className="flex flex-col">
+                            <li className='flex items-center border-b justify-between py-2'>
+                                <p className='text-md'>Martes</p>
+                                <p className="bg-blue-100 rounded-full w-30 h-8 flex justify-center items-center text-md">
+                                    16:00 - 17:00
+                                </p>
                             </li>
                         </ul>
-                        <ul>
-                            <li>
-                                <p>Martes</p>
-                                <p>16:00 - 17:00</p>
+                        <ul className="flex flex-col">
+                            <li className='flex items-center border-b justify-between py-2'>
+                                <p className='text-md'>Miercoles</p>
+                                <p className="bg-blue-100 rounded-full w-30 h-8 flex justify-center items-center text-md">
+                                    12:00 - 15:00
+                                </p>
                             </li>
                         </ul>
                     </div>
-                    <div>
+                    {/* Aviso */}
+                    <div className='text-lg pt-3 text-gray-400'>
                         <p>*Los horarios de los médicos se encuentran sujetos a variaciones sin previo aviso</p>
                     </div>
                 </div>
             </div>
-            
         </section>
     </>)
 }
--- src/pages/Medicos/components/InfoMedicos.jsx
+++ src/pages/Medicos/components/InfoMedicos.jsx
@@ -7,145 +7,145 @@ const InfoMedicos = () => {
         <section className="pt-10 pb-10">
             <div className="flex flex-wrap justify-around container m-auto gap-16">
                 {/* Item1 */}
-                <div className=" bg-white rounded-3xl shadow-md overflow-hidden max-w-sm">
-                    <img src={ImagenDoctorGenerica} alt="Doctor" className="w-full h-60 object-cover"/>
-                    <div className="p-4 space-y-3">
-                        <div className="bg-blue-100 text-sm font-semibold text-center rounded-lg py-1">
-                            <p>Cardiología</p>
-                        </div>
+                <div className="bg-white rounded-3xl shadow-md overflow-hidden max-w-sm">
+                    <img src={ImagenDoctorGenerica} alt="Doctor" className="w-full h-60 object-cover" />
+                    <div className="p-4 space-y-2">
+                        <p className="bg-blue-100 rounded-full w-30 h-8 flex justify-center items-center font-semibold text-sm">
+                            Cardiología
+                        </p>
                         <div>
                             <p className="font-bold text-gray-800">Dr. Martinez Quijandria Luis</p>
                             <p className="text-gray-400 text-sm pt-2">CMP: 55124</p>
                         </div>
-                        <a href="/detalle-medico" className="flex text-blue-600 justify-center items-center">
+                        <a href="/detalle-medico" className="flex text-blue-600 justify-center items-center pt-1">
                             <p className='pr-2'><b>Conoce al médico</b></p>
-                            <FontAwesomeIcon icon={faLongArrowRight}/>
+                            <FontAwesomeIcon icon={faLongArrowRight} />
                         </a>
                     </div>
                 </div>
 
                 {/* Item2 */}
-                <div className=" bg-white rounded-3xl shadow-md overflow-hidden">
-                    <img src={ImagenDoctorGenerica} alt="Doctor" className="w-full h-60 object-cover"/>
-                    <div className="p-4 space-y-3">
-                        <div className="bg-blue-100 text-sm font-semibold text-center rounded-lg py-1">
-                            <p>Cardiología</p>
-                        </div>
+                <div className="bg-white rounded-3xl shadow-md overflow-hidden max-w-sm">
+                    <img src={ImagenDoctorGenerica} alt="Doctor" className="w-full h-60 object-cover" />
+                    <div className="p-4 space-y-2">
+                        <p className="bg-blue-100 rounded-full w-30 h-8 flex justify-center items-center font-semibold text-sm">
+                            Cardiología
+                        </p>
                         <div>
                             <p className="font-bold text-gray-800">Dr. Martinez Quijandria Luis</p>
                             <p className="text-gray-400 text-sm pt-2">CMP: 55124</p>
                         </div>
-                        <a href="/detalle-medico" className="flex text-blue-600 justify-center items-center">
+                        <a href="/detalle-medico" className="flex text-blue-600 justify-center items-center pt-1">
                             <p className='pr-2'><b>Conoce al médico</b></p>
-                            <FontAwesomeIcon icon={faLongArrowRight}/>
+                            <FontAwesomeIcon icon={faLongArrowRight} />
                         </a>
                     </div>
                 </div>
 
                 {/* Item3 */}
-                <div className=" bg-white rounded-3xl shadow-md overflow-hidden">
-                    <img src={ImagenDoctorGenerica} alt="Doctor" className="w-full h-60 object-cover"/>
-                    <div className="p-4 space-y-3">
-                        <div className="bg-blue-100 text-sm font-semibold text-center rounded-lg py-1">
-                            <p>Cardiología</p>
-                        </div>
+                <div className="bg-white rounded-3xl shadow-md overflow-hidden max-w-sm">
+                    <img src={ImagenDoctorGenerica} alt="Doctor" className="w-full h-60 object-cover" />
+                    <div className="p-4 space-y-2">
+                        <p className="bg-blue-100 rounded-full w-30 h-8 flex justify-center items-center font-semibold text-sm">
+                            Cardiología
+                        </p>
                         <div>
                             <p className="font-bold text-gray-800">Dr. Martinez Quijandria Luis</p>
                             <p className="text-gray-400 text-sm pt-2">CMP: 55124</p>
                         </div>
-                        <a href="/detalle-medico" className="flex text-blue-600 justify-center items-center">
+                        <a href="/detalle-medico" className="flex text-blue-600 justify-center items-center pt-1">
                             <p className='pr-2'><b>Conoce al médico</b></p>
-                            <FontAwesomeIcon icon={faLongArrowRight}/>
+                            <FontAwesomeIcon icon={faLongArrowRight} />
                         </a>
                     </div>
                 </div>
 
                 {/* Item4 */}
-                <div className=" bg-white rounded-3xl shadow-md overflow-hidden">
-                    <img src={ImagenDoctorGenerica} alt="Doctor" className="w-full h-60 object-cover"/>
-                    <div className="p-4 space-y-3">
-                        <div className="bg-blue-100 text-sm font-semibold text-center rounded-lg py-1">
-                            <p>Cardiología</p>
-                        </div>
+                <div className="bg-white rounded-3xl shadow-md overflow-hidden max-w-sm">
+                    <img src={ImagenDoctorGenerica} alt="Doctor" className="w-full h-60 object-cover" />
+                    <div className="p-4 space-y-2">
+                        <p className="bg-blue-100 rounded-full w-30 h-8 flex justify-center items-center font-semibold text-sm">
+                            Cardiología
+                        </p>
                         <div>
                             <p className="font-bold text-gray-800">Dr. Martinez Quijandria Luis</p>
                             <p className="text-gray-400 text-sm pt-2">CMP: 55124</p>
                         </div>
-                        <a href="/detalle-medico" className="flex text-blue-600 justify-center items-center">
+                        <a href="/detalle-medico" className="flex text-blue-600 justify-center items-center pt-1">
                             <p className='pr-2'><b>Conoce al médico</b></p>
-                            <FontAwesomeIcon icon={faLongArrowRight}/>
+                            <FontAwesomeIcon icon={faLongArrowRight} />
                         </a>
                     </div>
                 </div>
 
                 {/* Item5 */}
-                <div className=" bg-white rounded-3xl shadow-md overflow-hidden">
-                    <img src={ImagenDoctorGenerica} alt="Doctor" className="w-full h-60 object-cover"/>
-                    <div className="p-4 space-y-3">
-                        <div className="bg-blue-100 text-sm font-semibold text-center rounded-lg py-1">
-                            <p>Cardiología</p>
-                        </div>
+                <div className="bg-white rounded-3xl shadow-md overflow-hidden max-w-sm">
+                    <img src={ImagenDoctorGenerica} alt="Doctor" className="w-full h-60 object-cover" />
+                    <div className="p-4 space-y-2">
+                        <p className="bg-blue-100 rounded-full w-30 h-8 flex justify-center items-center font-semibold text-sm">
+                            Cardiología
+                        </p>
                         <div>
                             <p className="font-bold text-gray-800">Dr. Martinez Quijandria Luis</p>
                             <p className="text-gray-400 text-sm pt-2">CMP: 55124</p>
                         </div>
-                        <a href="/detalle-medico" className="flex text-blue-600 justify-center items-center">
+                        <a href="/detalle-medico" className="flex text-blue-600 justify-center items-center pt-1">
                             <p className='pr-2'><b>Conoce al médico</b></p>
-                            <FontAwesomeIcon icon={faLongArrowRight}/>
+                            <FontAwesomeIcon icon={faLongArrowRight} />
                         </a>
                     </div>
                 </div>
 
                 {/* Item6 */}
-                <div className=" bg-white rounded-3xl shadow-md overflow-hidden">
-                    <img src={ImagenDoctorGenerica} alt="Doctor" className="w-full h-60 object-cover"/>
-                    <div className="p-4 space-y-3">
-                        <div className="bg-blue-100 text-sm font-semibold text-center rounded-lg py-1">
-                            <p>Cardiología</p>
-                        </div>
+                <div className="bg-white rounded-3xl shadow-md overflow-hidden max-w-sm">
+                    <img src={ImagenDoctorGenerica} alt="Doctor" className="w-full h-60 object-cover" />
+                    <div className="p-4 space-y-2">
+                        <p className="bg-blue-100 rounded-full w-30 h-8 flex justify-center items-center font-semibold text-sm">
+                            Cardiología
+                        </p>
                         <div>
                             <p className="font-bold text-gray-800">Dr. Martinez Quijandria Luis</p>
                             <p className="text-gray-400 text-sm pt-2">CMP: 55124</p>
                         </div>
-                        <a href="/detalle-medico" className="flex text-blue-600 justify-center items-center">
+                        <a href="/detalle-medico" className="flex text-blue-600 justify-center items-center pt-1">
                             <p className='pr-2'><b>Conoce al médico</b></p>
-                            <FontAwesomeIcon icon={faLongArrowRight}/>
+                            <FontAwesomeIcon icon={faLongArrowRight} />
                         </a>
                     </div>
                 </div>
 
                 {/* Item7 */}
-                <div className=" bg-white rounded-3xl shadow-md overflow-hidden">
-                    <img src={ImagenDoctorGenerica} alt="Doctor" className="w-full h-60 object-cover"/>
-                    <div className="p-4 space-y-3">
-                        <div className="bg-blue-100 text-sm font-semibold text-center rounded-lg py-1">
-                            <p>Cardiología</p>
-                        </div>
+                <div className="bg-white rounded-3xl shadow-md overflow-hidden max-w-sm">
+                    <img src={ImagenDoctorGenerica} alt="Doctor" className="w-full h-60 object-cover" />
+                    <div className="p-4 space-y-2">
+                        <p className="bg-blue-100 rounded-full w-30 h-8 flex justify-center items-center font-semibold text-sm">
+                            Cardiología
+                        </p>
                         <div>
                             <p className="font-bold text-gray-800">Dr. Martinez Quijandria Luis</p>
                             <p className="text-gray-400 text-sm pt-2">CMP: 55124</p>
                         </div>
-                        <a href="/detalle-medico" className="flex text-blue-600 justify-center items-center">
+                        <a href="/detalle-medico" className="flex text-blue-600 justify-center items-center pt-1">
                             <p className='pr-2'><b>Conoce al médico</b></p>
-                            <FontAwesomeIcon icon={faLongArrowRight}/>
+                            <FontAwesomeIcon icon={faLongArrowRight} />
                         </a>
                     </div>
                 </div>
 
                 {/* Item8 */}
-                <div className=" bg-white rounded-3xl shadow-md overflow-hidden">
-                    <img src={ImagenDoctorGenerica} alt="Doctor" className="w-full h-60 object-cover"/>
-                    <div className="p-4 space-y-3">
-                        <div className="bg-blue-100 text-sm font-semibold text-center rounded-lg py-1">
-                            <p>Cardiología</p>
-                        </div>
+                <div className="bg-white rounded-3xl shadow-md overflow-hidden max-w-sm">
+                    <img src={ImagenDoctorGenerica} alt="Doctor" className="w-full h-60 object-cover" />
+                    <div className="p-4 space-y-2">
+                        <p className="bg-blue-100 rounded-full w-30 h-8 flex justify-center items-center font-semibold text-sm">
+                            Cardiología
+                        </p>
                         <div>
                             <p className="font-bold text-gray-800">Dr. Martinez Quijandria Luis</p>
                             <p className="text-gray-400 text-sm pt-2">CMP: 55124</p>
                         </div>
-                        <a href="/detalle-medico" className="flex text-blue-600 justify-center items-center">
+                        <a href="/detalle-medico" className="flex text-blue-600 justify-center items-center pt-1">
                             <p className='pr-2'><b>Conoce al médico</b></p>
-                            <FontAwesomeIcon icon={faLongArrowRight}/>
+                            <FontAwesomeIcon icon={faLongArrowRight} />
                         </a>
                     </div>
                 </div>
