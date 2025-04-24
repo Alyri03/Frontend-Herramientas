14abc176bfdf973736a5a7a57572991488fb7a47
JuanGarcia <juan2525.2004@gmail.com>
2025-04-24 00:58:36 -0500

    Pagina DetalleMedico terminada | Responsive

--- src/pages/Medicos/DetalleMedicos.jsx
+++ src/pages/Medicos/DetalleMedicos.jsx
@@ -5,78 +5,78 @@ import { faLongArrowLeft } from '@fortawesome/free-solid-svg-icons';
 const DetalleMedico = () => {
     return (<>
         <section className='pt-10 pb-10'>
-            <div className="flex justify-center gap-10">
+            <div className="lg:flex justify-center align-center gap-10">
                 {/* Imagen | Nav */}
-                <div className="flex flex-col w-3xl justify-center items-center relative">
+                <div className="flex flex-col justify-center items-center relative h-auto 2xl:w-[800px] xl:w-[700px] lg:w-[600px] lg:p-0 sm:p-10 p-5">
                     <img src={ImagenDoctorGenerica} alt="doctor" className="w-full object-cover" />
-                    <div className="absolute bottom-0 left-0 w-full flex justify-between items-center border border-stone-200 bg-white border-t rounded-t-3xl px-5 py-3 z-10">
+                    <div className="absolute bottom-0 w-full flex justify-between items-center border border-stone-200 bg-white border-t rounded-t-3xl sm:px-5 px-3 sm:py-3 py-2">
                         <a href="/equipo-medico" className="flex items-center text-blue-900 font-medium">
                             <FontAwesomeIcon icon={faLongArrowLeft} className="mr-2" />
-                            <p className='text-lg'>Volver al listado</p>
+                            <p className='lg:text-lg md:text-md sm:text-sm text-[13px]'>Volver al listado</p>
                         </a>
-                        <a href="#" className="bg-cyan-700 text-white rounded-full w-45 h-10 flex justify-center items-center font-semibold hover:bg-cyan-800 transition text-lg">
+                        <a href="#" className="bg-cyan-700 text-white rounded-full lg:w-45 md:w-40 sm:w-35 lg:h-10 md:h-9 sm:h-8 w-30 h-7 flex justify-center items-center font-semibold hover:bg-cyan-800 transition lg:text-lg md:text-md sm:text-sm text-[13px]">
                             Reservar Cita
                         </a>
                     </div>
                 </div>
 
                 {/* Info Doctor */}
-                <div className='w-[45%] h-auto'>
+                <div className='2xl:w-[750px] xl:w-[650px] lg:w-[550px] h-auto lg:p-0 sm:p-10 p-5'>
                     <div className='flex flex-col'>
-                        <p className='text-6xl pt-2'><b>Dr. Reyes Pérez, Alvaro Orlando</b></p>
-                        <div className='flex gap-20 pt-3 pb-3'>
-                            <p className='text-2xl text-gray-400'>CMP: 55124</p>
-                            <p className='text-2xl text-gray-400'>RNE: 032319</p>
+                        <p className='2xl:text-6xl xl:text-5xl sm:text-2xl xl:pt-2 sm:pt-1 text-2xl pt-1'><b>Dr. Reyes Pérez, Alvaro Orlando</b></p>
+                        <div className='flex gap-20 2xl:pt-3 xl:pt-2 sm:pt-1 2xl:pb-3 xl:pb-2 sm:pb-1 pt-1 pb-1'>
+                            <p className='2xl:text-2xl xl:text-xl sm:text-md text-md text-gray-400'>CMP: 55124</p>
+                            <p className='2xl:text-2xl xl:text-xl sm:text-md text-md text-gray-400'>RNE: 032319</p>
                         </div>
                     </div>
                     {/* Especialidades */}
-                    <div className='pt-3 pb-3'>
-                        <p className='text-3xl'><b>Especialidades</b></p>
-                        <div className='pt-3'>
-                            <p className="bg-blue-100 rounded-full w-45 h-10 flex justify-center items-center font-semibold text-lg">
+                    <div className='2xl:pt-3 xl:pt-2 sm:pt-1 2xl:pb-3 xl:pb-2 sm:pb-1 pt-1 pb-1'>
+                        <p className='2xl:text-3xl xl:text-2xl sm:text-md text-md'><b>Especialidades</b></p>
+                        <div className='2xl:pt-3 xl:pt-2 sm:pt-1 pt-1'>
+                            <p className="bg-blue-100 rounded-full flex justify-center items-center font-semibold 2xl:w-45 xl:w-40 sm:w-30 2xl:h-10 xl:h-9 lg:h-7 2xl:text-lg xl:text-md sm:text-sm text-sm w-30 h-7">
                                 Cardiología
                             </p>
                         </div>
                     </div>
                     {/* Sobre mí */}
-                    <div className='pt-3 pb-3'>
-                        <p className='text-3xl'><b>Sobre mí</b></p>
-                        <p className='text-xl pt-3'>Soy miembro de La Sociedad Estadounidense de Cirujanos Plásticos. A lo largo de mi carrera en el campo de la cirugía plástica y estética, he participado en una amplia gama de actividades educativas y eventos científicos entre las cuales destacan:</p>
-                        <ul className='text-xl pt-3 list-disc list-outside pl-10'>
+                    <div className='2xl:pt-3 xl:pt-2 sm:pt-1 2xl:pb-3 xl:pb-2 sm:pb-1 pt-1 pb-1'>
+                        <p className='2xl:text-3xl xl:text-2xl sm:text-md text-md'><b>Sobre mí</b></p>
+                        <p className='2xl:text-xl xl:text-lg sm:text-sm 2xl:pt-3 xl:pt-2 sm:pt-1 text-sm pt-1'>Soy miembro de La Sociedad Estadounidense de Cirujanos Plásticos. A lo largo de mi carrera en el campo de la cirugía plástica y estética, he participado en una amplia gama de actividades educativas y eventos científicos entre las cuales destacan:</p>
+                        <ul className='2xl:text-xl xl:text-lg sm:text-sm 2xl:pt-3 xl:pt-2 sm:pt-1 text-sm pt-1 list-disc list-outside pl-10'>
                             <li>Anatomy of Aging Bitesized y Clinical Practice</li>
                             <li>Transplante facial de La Sociedad Estadounidense de Cirujanos Plásticos.</li>
                         </ul>
                     </div>
                     {/* Formación */}
-                    <div className='pt-3 pb-3'>
-                        <p className='text-3xl'><b>Formación</b></p>
-                        <ul className='text-xl pt-3 list-disc list-outside pl-10'>
+                    <div className='2xl:pt-3 xl:pt-2 sm:pt-1 2xl:pb-3 xl:pb-2 sm:pb-1 pt-1 pb-1'>
+                        <p className='2xl:text-3xl xl:text-2xl sm:text-md text-md'><b>Formación</b></p>
+                        <ul className='2xl:text-xl xl:text-lg sm:text-sm text-sm 2xl:pt-3 xl:pt-2 sm:pt-1 pt-1 list-disc list-outside pl-10'>
                             <li>Médico Cirujano de Novgorod State University. (2002-2008)</li>
                             <li>Cirujano Plástico, Estético y Reconstructivo de la Universidad de Ciencias Médicas de La Habana. (2013-2017)</li>
                         </ul>
                     </div>
                     {/* Horarios */}
-                    <div className='pt-3 pb-3'>
-                        <p className='text-3xl'><b>Horarios</b></p>
-                        <ul className="flex flex-col">
+                    <div className='2xl:pt-3 xl:pt-2 sm:pt-1 2xl:pb-3 xl:pb-2 sm:pb-1 pt-1 pb-1'>
+                        <p className='2xl:text-3xl xl:text-2xl sm:text-md text-md'><b>Horarios</b></p>
+                        <ul className="flex flex-col text-md">
                             <li className='flex items-center border-b justify-between py-2'>
-                                <p className='text-md'>Martes</p>
-                                <p className="bg-blue-100 rounded-full w-30 h-8 flex justify-center items-center text-md">
+                                <p>Martes</p>
+                                <p className="bg-blue-100 rounded-full w-30 h-8 flex justify-center items-center">
                                     16:00 - 17:00
                                 </p>
                             </li>
                         </ul>
-                        <ul className="flex flex-col">
+                        <ul className="flex flex-col text-md">
                             <li className='flex items-center border-b justify-between py-2'>
-                                <p className='text-md'>Miercoles</p>
-                                <p className="bg-blue-100 rounded-full w-30 h-8 flex justify-center items-center text-md">
+                                <p>Miercoles</p>
+                                <p className="bg-blue-100 rounded-full w-30 h-8 flex justify-center items-center">
                                     12:00 - 15:00
                                 </p>
                             </li>
                         </ul>
                     </div>
                     {/* Aviso */}
-                    <div className='text-lg pt-3 text-gray-400'>
+                    <div className='2xl:text-lg xl:text-md sm:text-sm text-sm 2xl:pt-3 xl:pt-2 sm:pt-1 pt-1 text-gray-400 '>
                         <p>*Los horarios de los médicos se encuentran sujetos a variaciones sin previo aviso</p>
                     </div>
                 </div>
