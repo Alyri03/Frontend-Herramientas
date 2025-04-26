397d1bd716a6174ae2127737c922249a3be9cbb6
JuanGarcia <juan2525.2004@gmail.com>
2025-04-25 20:54:05 -0500

    Mejora DetalleMedicos

--- src/pages/Medicos/DetalleMedicos.jsx
+++ src/pages/Medicos/DetalleMedicos.jsx
@@ -4,11 +4,11 @@ import { faLongArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
 const DetalleMedico = () => {
     return (<>
-        <section className='pt-10 pb-10'>
+        <section className='lg:pt-5 p-1 pb-10'>
             <div className="lg:flex justify-center align-center gap-10">
                 {/* Imagen | Nav */}
                 <div className="flex flex-col justify-center items-center relative h-auto 2xl:w-[800px] xl:w-[700px] lg:w-[600px] lg:p-0 sm:p-10 p-5">
-                    <img src={ImagenDoctorGenerica} alt="doctor" className="w-full object-cover" />
+                    <img src={ImagenDoctorGenerica} alt="doctor" className="w-full object-cover"/>
                     <div className="absolute bottom-0 w-full flex justify-between items-center border border-stone-200 bg-white border-t rounded-t-3xl sm:px-5 px-3 sm:py-3 py-2">
                         <a href="/equipo-medico" className="flex items-center text-blue-900 font-medium">
                             <FontAwesomeIcon icon={faLongArrowLeft} className="mr-2" />
@@ -21,19 +21,19 @@ const DetalleMedico = () => {
                 </div>
 
                 {/* Info Doctor */}
-                <div className='2xl:w-[750px] xl:w-[650px] lg:w-[550px] h-auto lg:p-0 sm:p-10 p-5'>
+                <div className='2xl:w-[750px] xl:w-[650px] lg:w-[550px] h-auto lg:p-0 sm:pl-10 sm:pr-10 p-5'>
                     <div className='flex flex-col'>
-                        <p className='2xl:text-6xl xl:text-5xl sm:text-2xl xl:pt-2 sm:pt-1 text-2xl pt-1'><b>Dr. Reyes Pérez, Alvaro Orlando</b></p>
+                        <p className='2xl:text-5xl xl:text-4xl sm:text-2xl xl:pt-2 sm:pt-1 text-2xl pt-1'><b>Dr. Reyes Pérez, Alvaro Orlando</b></p>
                         <div className='flex gap-20 2xl:pt-3 xl:pt-2 sm:pt-1 2xl:pb-3 xl:pb-2 sm:pb-1 pt-1 pb-1'>
-                            <p className='2xl:text-2xl xl:text-xl sm:text-md text-md text-gray-400'>CMP: 55124</p>
-                            <p className='2xl:text-2xl xl:text-xl sm:text-md text-md text-gray-400'>RNE: 032319</p>
+                            <p className='2xl:text-2xl xl:text-lg sm:text-md text-md text-gray-400'>CMP: 55124</p>
+                            <p className='2xl:text-2xl xl:text-lg sm:text-md text-md text-gray-400'>RNE: 032319</p>
                         </div>
                     </div>
                     {/* Especialidades */}
                     <div className='2xl:pt-3 xl:pt-2 sm:pt-1 2xl:pb-3 xl:pb-2 sm:pb-1 pt-1 pb-1'>
                         <p className='2xl:text-3xl xl:text-2xl sm:text-md text-md'><b>Especialidades</b></p>
                         <div className='2xl:pt-3 xl:pt-2 sm:pt-1 pt-1'>
-                            <p className="bg-blue-100 rounded-full flex justify-center items-center font-semibold 2xl:w-45 xl:w-40 sm:w-30 2xl:h-10 xl:h-9 lg:h-7 2xl:text-lg xl:text-md sm:text-sm text-sm w-30 h-7">
+                            <p className="bg-blue-100 rounded-full flex justify-center items-center font-semibold 2xl:w-45 xl:w-45 lg:w-35 sm:w-30 2xl:h-10 xl:h-9 lg:h-7 2xl:text-lg xl:text-lg lg:text-md sm:text-sm text-sm w-30 h-7">
                                 Cardiología
                             </p>
                         </div>
