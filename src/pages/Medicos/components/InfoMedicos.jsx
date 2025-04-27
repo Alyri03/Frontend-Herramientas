d0827f69236b46b463626d050c17fdb58fc7ef58
JuanGarcia <juan2525.2004@gmail.com>
2025-04-27 14:01:32 -0500

    Mejora en InfoMedicos

--- src/pages/Medicos/components/InfoMedicos.jsx
+++ src/pages/Medicos/components/InfoMedicos.jsx
@@ -1,10 +1,10 @@
-import CardMedico from "./cardMedico"
+import CardMedico from "./CardMedico"
 
 const InfoMedicos = ({ medicoss }) => {
 
     return (<>
         <section className="pt-10 pb-10">
-            <div className="flex flex-wrap justify-around container m-auto sm:gap-9 gap-7">
+            <div className="flex flex-wrap justify-evenly container m-auto sm:gap-10 gap-7">
                 {medicoss.map((doctores) => (
                     <CardMedico
                         key={doctores.id}
