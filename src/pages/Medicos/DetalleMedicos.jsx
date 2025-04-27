2fbfa97268c9b029933cf9efaca3ccc699b5ed9f
JuanGarcia <juan2525.2004@gmail.com>
2025-04-27 14:15:07 -0500

    Correción en DetalleMedico

--- src/pages/Medicos/DetalleMedicos.jsx
+++ src/pages/Medicos/DetalleMedicos.jsx
@@ -2,6 +2,7 @@ import ImagenDoctorGenerica from '../../assets/images/ImagenDoctorGenerica.jpg'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLongArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import Preefooter from '../../components/Preefooter';
+import { Link } from 'react-router-dom';
 
 const DetalleMedico = () => {
     return (<>
@@ -11,13 +12,13 @@ const DetalleMedico = () => {
                 <div className="flex flex-col justify-center items-center relative h-auto 2xl:w-[800px] xl:w-[700px] lg:w-[600px] lg:p-0 sm:p-10 p-5" data-aos="fade-right">
                     <img src={ImagenDoctorGenerica} alt="doctor" className="w-full object-cover" />
                     <div className="absolute bottom-0 w-full flex justify-between items-center border border-stone-200 bg-white border-t rounded-t-3xl sm:px-5 px-3 sm:py-3 py-2">
-                        <a href="/equipo-medico" className="flex items-center text-blue-900 font-medium">
+                        <Link to="/equipo-medico" className="flex items-center text-blue-900 font-medium">
                             <FontAwesomeIcon icon={faLongArrowLeft} className="mr-2" />
                             <p className='lg:text-lg md:text-md sm:text-sm text-[13px]'>Volver al listado</p>
-                        </a>
-                        <a href="#" className="bg-cyan-700 text-white rounded-full lg:w-45 md:w-40 sm:w-35 lg:h-10 md:h-9 sm:h-8 w-30 h-7 flex justify-center items-center font-semibold hover:bg-cyan-800 transition lg:text-lg md:text-md sm:text-sm text-[13px]">
+                        </Link>
+                        <Link to="#" className="bg-cyan-700 text-white rounded-full lg:w-45 md:w-40 sm:w-35 lg:h-10 md:h-9 sm:h-8 w-30 h-7 flex justify-center items-center font-semibold hover:bg-cyan-800 transition lg:text-lg md:text-md sm:text-sm text-[13px]">
                             Reservar Cita
-                        </a>
+                        </Link>
                     </div>
                 </div>
 
