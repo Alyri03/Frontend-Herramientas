7a82a70baafdd393c54f0d5edf1ab212296239e7
Luis Martínez <lmartinezquijandria03@gmail.com>
2025-06-03 21:14:34 -0500

    Uso de react query

--- src/pages/pagina-publica/Servicios/PaginaServicios.jsx
+++ src/pages/pagina-publica/Servicios/PaginaServicios.jsx
@@ -1,31 +1,18 @@
-import React, { useEffect, useState } from "react";
 import CardServicio from "@/pages/pagina-publica/Servicios/components/CardServicio";
 import Preefooter from "@/components/Preefooter";
 import Spinner from "@/components/Spinner";
-import { obtenerServicios } from "@/services/servicioService";
+import { useDatosServicios } from "@/hooks/useDatosServicios";
 
 const PaginaServicios = () => {
-  const [servicios, setServicios] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const cargarServicios = async () => {
-      try {
-        const data = await obtenerServicios();
-        setServicios(data);
-      } catch (error) {
-        console.error("Error al cargar servicios:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    cargarServicios();
-  }, []);
+  const {
+    servicios,
+    cargandoServicios,
+    errorServicios,
+  } = useDatosServicios();
 
   return (
     <div className="p-4 max-w-7xl mx-auto">
-      <div className="mb-6 px-4 py-16 " data-aos="fade-down">
+      <div className="mb-6 px-4 py-16" data-aos="fade-down">
         <h1 className="text-2xl md:text-3xl font-bold text-gray-900 leading-tight text-left max-w-4xl mx-auto">
           Contamos con una amplia
           <br className="hidden sm:block" />
@@ -35,10 +22,14 @@ const PaginaServicios = () => {
         </h1>
       </div>
 
-      {loading ? (
+      {cargandoServicios ? (
         <div className="flex justify-center py-10">
           <Spinner size={10} />
         </div>
+      ) : errorServicios ? (
+        <p className="text-center text-red-500 py-10">
+          Error al cargar los servicios.
+        </p>
       ) : (
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
           {servicios.map((servicio, index) => (
