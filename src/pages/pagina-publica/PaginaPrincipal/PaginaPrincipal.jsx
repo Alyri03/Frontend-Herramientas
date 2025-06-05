e6f9fd9f7a8dfd0dde53b4a2ac73343c3b235300
Luis Enrique <lmartinezquijandria@gmail.com>
2025-06-05 10:13:04 -0500

    Probando CICD con Jenkins 5

--- src/pages/pagina-publica/PaginaPrincipal/PaginaPrincipal.jsx
+++ src/pages/pagina-publica/PaginaPrincipal/PaginaPrincipal.jsx
@@ -7,7 +7,7 @@ import Aseguradoras from '@/components/Aseguradoras';
 import Preefooter from '@/components/Preefooter';
 
 const PaginaPrincipal = () => {
-    console.log("Probando CICD con Jenkins 4")
+    console.log("Probando CICD con Jenkins 5")
     return (
         <>
             <Carrusel />
